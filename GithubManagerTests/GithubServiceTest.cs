5243d1766b59c0760e876a1283c2a49052c86a26
Nikolay Dzencharskii (Akvelon) <v-nidzen@microsoft.com>
2016-06-21 14:45:03 +0300

    Fixed unit test

--- GithubManagerTests/GithubServiceTest.cs
+++ GithubManagerTests/GithubServiceTest.cs
@@ -1,17 +1,18 @@
-﻿using GithubManager.Services;
+﻿using System.Threading.Tasks;
+using GithubManager.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GithubManagerTests
 {
     [TestClass]
-    public class GithubClientTests
+    public class GithubServiceTest
     {
         [TestMethod]
-        public async void CorrectDescriptionGetting()
+        public async Task CorrectDescriptionGetting()
         {
             var c = new GithubService();
             var description = await c.GetDescription("octokit", "octokit.net");
-            Assert.AreEqual(description, "\"A GitHub API client library for .NET \"") ;
+            Assert.AreEqual(description, "A GitHub API client library for .NET ");
         }
     }
 }
